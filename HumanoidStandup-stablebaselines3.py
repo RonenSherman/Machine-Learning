3e0c2382712ae4abc3b2580e4d6a0608ba4d3dfe
shermanr <roshersnowy@gmail.com>
2026-09-22 09:23:08 -0700

    updates

--- HumanoidStandup-stablebaselines3.py
+++ HumanoidStandup-stablebaselines3.py
@@ -4,20 +4,13 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-vec_env = make_vec_env("HumanoidStandup-v5", n_envs=32)
+vec_env = make_vec_env("HumanoidStandup-v5", n_envs=16)
 
-model = PPO.load("ppo_HumanoidStandup-v5")
-model.set_env(vec_env)
-
+model = PPO("MlpPolicy", vec_env, verbose=1)
 model.learn(total_timesteps=1000000)
-#while model.learn:
-#    model.save("ppo_HumanoidStandup-v5")
-#    print("model saved")
-
 model.save("ppo_HumanoidStandup-v5")
-print("model saved")
-del model
 
+del model # remove to demonstrate saving and loading
 
 model = PPO.load("ppo_HumanoidStandup-v5")
 
